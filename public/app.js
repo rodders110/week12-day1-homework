97721875b64551126bbb3e4ed312d5cc71b49556
Roddy <roddy_daly110@hotmail.com>
2018-07-30 21:05:21 +0100

    all cats listed dynamically

--- public/app.js
+++ public/app.js
@@ -25,8 +25,13 @@ var addCat = function(name, favourite_food, image){
 }
 
 window.onload = function(){
-
-    addCat('Garfield', 'lasagne', 'https://vignette.wikia.nocookie.net/villainstournament/images/f/f2/Garfield.jpg/revision/latest?cb=20141219205245');
+    var cattery = catData();
+    console.log(cattery);
+    for (let item of cattery){
+        
+        addCat(item.name, item.fav_food, item.image );
+    };
+    
    }
 
 var createCatList = function (){
@@ -71,15 +76,16 @@ var addToPage = function(catList, catName, fav_food, imageList, catImage){
 
 var catData = function(name, favFood, image){
     var array = [];
-    let cat = function(){
+    let Cat = function(name, favFood, image){
         this.name = name;
         this.fav_food = favFood;
         this.image = image;
     }
-    array.push(cat("Boba", "Sock fluff", 'http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg'));
-    array.push(cat("Barnaby", "Tuna", 'https://68.media.tumblr.com/88d0fcf2b84a7b098dda839130597569/tumblr_okuo4teiql1uhevdso1_1280.jpg'));
-    array.push(cat("Max", "Whiskas Temptations", 'http://66.media.tumblr.com/7c5784ea89369c780e782bf10c60315a/tumblr_npb0hlYwhV1u63jaco1_1280.jpg'));
-    array.push(cat("Garfield", "Lasagne", 'https://vignette.wikia.nocookie.net/villainstournament/images/f/f2/Garfield.jpg/revision/latest?cb=20141219205245'))
+    array.push(cat1 = new Cat("Boba", "Sock fluff", 'http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg'));
+    array.push(cat2 = new Cat("Barnaby", "Tuna", 'https://68.media.tumblr.com/88d0fcf2b84a7b098dda839130597569/tumblr_okuo4teiql1uhevdso1_1280.jpg'));
+    array.push(cat3 = new Cat("Max", "Whiskas Temptations", 'http://66.media.tumblr.com/7c5784ea89369c780e782bf10c60315a/tumblr_npb0hlYwhV1u63jaco1_1280.jpg'));
+    array.push(cat4 = new Cat("Garfield", "Lasagne", 'https://vignette.wikia.nocookie.net/villainstournament/images/f/f2/Garfield.jpg/revision/latest?cb=20141219205245'))
+    return array;
 }
 
 
